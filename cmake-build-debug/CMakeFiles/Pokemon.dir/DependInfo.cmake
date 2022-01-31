e40e2356eb9df0c25a20ccad220a50f06e9cd221
Evan Williams <evskimo97@gmail.com>
2022-01-31 08:58:23 +0000

    Change some minor dialogue

---
+++ cmake-build-debug/CMakeFiles/Pokemon.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/evski/Documents/TUD Games Design/Year 3/Own Projects/C++/CPPokemon/main.cpp" "CMakeFiles/Pokemon.dir/main.cpp.obj" "gcc" "CMakeFiles/Pokemon.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/evski/Documents/TUD Games Design/Year 3/Own Projects/C++/Pokemon
+# Install script for directory: C:/Users/evski/Documents/TUD Games Design/Year 3/Own Projects/C++/CPPokemon
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/evski/Documents/TUD Games Design/Year 3/Own Projects/C++/Pokemon/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/evski/Documents/TUD Games Design/Year 3/Own Projects/C++/CPPokemon/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
